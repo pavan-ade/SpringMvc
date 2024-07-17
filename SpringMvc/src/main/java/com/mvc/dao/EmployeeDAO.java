1876d17b14d3c09e2b22571d8bfe99ddc33d1dc6
pavan-ade <pavanade09@gmail.com>
2024-07-17 22:01:43 +0530

    create new Branch of searching Employee with id,name,address and Salary

--- SpringMvc/src/main/java/com/mvc/dao/EmployeeDAO.java
+++ SpringMvc/src/main/java/com/mvc/dao/EmployeeDAO.java
@@ -5,9 +5,11 @@ import java.util.List;
 import com.mvc.model.Employee;
 
 public interface EmployeeDAO {
+	
 	public List<Employee> getAllEmployee();
 	public void saveEmployee(Employee emp);
 	public void deleteEmployee(Integer id);
 	public void updateEmployee(Employee emp);
 	public Employee getEmployeeById(Integer id);
+	
 }
